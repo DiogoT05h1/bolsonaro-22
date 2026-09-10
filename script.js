3a088fcd2cd24261e19c770f391c081641431454
DiogoT05h1 <toshimitsu.diogo@escola.pr.gov.br>
2026-09-10 07:50:07 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [
+  {
+    enunciado:
+      "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+    alternativas: [
+      {
+        texto: "Isso é assustador!",
+        afirmacao: [
+          "No início ficou com medo do que essa tecnologia pode fazer. ",
+          "Achou assustador pensar na velocidade na qual a tecnologia está avançando.",
+        ],
+      },
+      {
+        texto: "Isso é maravilhoso!",
+        afirmacao: [
+          "Quis saber como usar IA no seu dia a dia.",
+          "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia.",
+        ],
+      },
+    ],
+  },
+];
+const caixaPerguntas = document(".caixa-perguntas");
+        texto: "Isso é assustador!",
+        afirmacao: [
+            "No início ficou com medo do que essa tecnologia pode fazer.",
+            "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+        ]
+    },
+    {
+        texto: "Isso é maravilhoso!",
+        afirmacao: [
+            "Quis saber como usar IA no seu dia a dia.",
+            "Pensou que IA pode ajudar em tarefas da sua vida."
+        ]
+    }
+]
+function aleatorio (lista){
+        const posicao = Math.floor(Math.random()* lista.length);
+        return lista[posicao];
+}
+function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+        historiaFinal += afirmacoes + " ";
+        atual++;
+        mostraPergunta();
+}
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
+function jogaNovamente(){
+        atual = 0;
+        historiaFinal = "";
+        mostraPergunta();
+}
+function mostraResultado() {
+        caixaPerguntas.textContent = "Em 2049...";
+        textoResultado.textContent = historiaFinal;
+        caixaAlternativas.textContent = "";
+        botaoJogarNovamente.addEventListener("click", jogaNovamente());
+}
