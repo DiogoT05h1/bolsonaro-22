1bf9b57781d9b7234d4c796219fd9b8f56839408
DiogoT05h1 <toshimitsu.diogo@escola.pr.gov.br>
2026-09-11 09:22:57 -0300

    Add alternativas array with user responses

--- perguntas.js
+++ perguntas.js
@@ -39,3 +39,25 @@
                 },
         ]
 }
+
+alternativas: [ 
+
+  { 
+
+    texto: "Isso é assustador!", 
+
+    afirmacao: [ 
+
+      "No início ficou com medo do que essa tecnologia pode fazer.", 
+
+      "Achou assustador pensar na velocidade na qual a tecnologia está avançando.", 
+
+    ], 
+
+    proxima: 1, 
+
+  }, 
+
+ 
+
+]; 
